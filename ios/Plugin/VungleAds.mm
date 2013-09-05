a253db99d378c9011c5553e2f243a4bf2482838e
Eric Chen <eric.chen@vungle.com>
2013-09-05 11:18:24 -0700

    incremented Corona plugin version to 1.1.1 (for Android isAdAvailable() fix)

--- ios/Plugin/VungleAds.mm
+++ ios/Plugin/VungleAds.mm
@@ -36,7 +36,7 @@ static const NSString* kEVENT_TYPE_KEY = @"type";
 static const NSString* kAD_START_EVENT_TYPE = @"adStart";
 static const NSString* kAD_VIEW_EVENT_TYPE = @"adView";
 static const NSString* kAD_END_EVENT_TYPE = @"adEnd";
-static const NSString* kVERSION = @"1.1.0";
+static const NSString* kVERSION = @"1.1.1";
 
 // ----------------------------------------------------------------------------
 
