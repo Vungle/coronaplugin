fa2c2897a590234119064d6b6300793a4bce115a
Rolando Abarca <rolando.abarca@vungle.com>
2013-09-13 15:30:09 -0700

    updates iOS SDK and updates version to 1.1.2

--- ios/vunglepub/headers/VGConstants.h
+++ ios/vunglepub/headers/VGConstants.h
@@ -31,7 +31,7 @@
  */
 
 
-#define kVGInternalBuildNumber @"1.4.2"
+#define kVGInternalBuildNumber @"1.4.3"
 #define kStatusBarOffset 20
 #define kAdViewUpwardAnimDuration   0.5
 #define kAdViewForMovieDuration     0.8
