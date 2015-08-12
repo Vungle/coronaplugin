de4f23df67ab68f95cd3d6721c32418b7a1f7d75
kosyakow <kosyakow@gmail.com>
2015-08-12 17:58:00 +0300

    In adView add totalAdSeconds. In adEnd add wasCallToActionClicked

--- ios/Plugin/VungleAds.mm
+++ ios/Plugin/VungleAds.mm
@@ -51,9 +51,10 @@ int luaopen_CoronaProvider_ads_vungle( lua_State *L )
 
 - (void)vungleSDKwillCloseAdWithViewInfo:(NSDictionary *)viewInfo willPresentProductSheet:(BOOL)willPresentProductSheet {
     NSNumber* playTime = [viewInfo objectForKey:@"playTime"] ? [viewInfo objectForKey:@"playTime"] : @(0);
-    vungle->DispatchEvent(false, [kAD_VIEW_EVENT_TYPE UTF8String], @{@"secondsWatched": playTime, @"isCompletedView": [viewInfo objectForKey:@"completedView"]});
+    NSNumber* allTime = [viewInfo objectForKey:@"videoLength"] ? [viewInfo objectForKey:@"videoLength"] : @(0);
+    vungle->DispatchEvent(false, [kAD_VIEW_EVENT_TYPE UTF8String], @{@"secondsWatched": playTime, @"isCompletedView": [viewInfo objectForKey:@"completedView"], @"totalAdSeconds": allTime});
     if (!willPresentProductSheet) {
-        vungle->DispatchEvent(false, [kAD_END_EVENT_TYPE UTF8String]);
+        vungle->DispatchEvent(false, [kAD_END_EVENT_TYPE UTF8String], @{@"wasCallToActionClicked": [viewInfo objectForKey:@"didDownlaod"] ? @YES : @NO});
     }
 }
 
