937e400df32a5b5b5d8dfa585400c7f69a9a6944
AndreyK <kosyakow@gmail.com>
2015-09-03 10:16:12 +0300

    Add android libs to repo. Change android plugin ver to 3.3.2

--- android/src/CoronaProvider/ads/vungle/LuaLoader.java
+++ android/src/CoronaProvider/ads/vungle/LuaLoader.java
@@ -50,7 +50,7 @@ import com.vungle.log.Logger;
  */
 public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 	private static final String TAG = "VungleCorona";
-	private static final String VERSION = "2.1.2";
+	private static final String VERSION = "3.3.2";
 	private static final Locale LOCALE = Locale.US;
 
 	// LUA method names
