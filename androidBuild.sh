ecdafd11c702dad3897f878499dc7d9ad069c4e8
AndreyK <kosyakow@gmail.com>
2015-09-08 14:32:31 +0300

    Fix script

--- androidBuild.sh
+++ androidBuild.sh
@@ -1,11 +1,10 @@
 set -ve
 pwd
 cd ..
-[ -f ./corona-test-app ] && rm -rf ./corona-test-app
+[ -d ./corona-test-app ] && rm -rf ./corona-test-app
 git clone git@github.com:Vungle/corona-test-app.git
 cd coronaplugin
 cd android
 ./build.sh ~/Library/Android/sdk/
 cd ..
-cd ..
-rm -rf ../corona-test-app
+[ -d ../corona-test-app ] && rm -rf ../corona-test-app
