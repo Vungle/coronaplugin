db399e48374bb418aab8634d9cf527c1f10d7a9f
kosyakow <kosyakow@gmail.com>
2016-04-27 15:47:20 +0300

    setTransitionAnimationEnabled(false)

--- android/src/CoronaProvider/ads/vungle/LuaLoader.java
+++ android/src/CoronaProvider/ads/vungle/LuaLoader.java
@@ -395,6 +395,7 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 		final int PARAM_TABLE_INDEX = 2;
 		final int numberOfArguments = luaState.getTop();
 		final AdConfig adConfig = new AdConfig();
+		config.setTransitionAnimationEnabled(false);
 		// get the lower case ad type if it exists:
 		final String adType = 
 			(numberOfArguments >= AD_TYPE_INDEX ? 
@@ -475,7 +476,8 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
     public int showEx(LuaState luaState) {
         final String METHOD_NAME = SHOW_METHOD + "(): ";
         final AdConfig adConfig = new AdConfig();
-        final int numberOfArguments = luaState.getTop();
+	config.setTransitionAnimationEnabled(false);
+	final int numberOfArguments = luaState.getTop();
         // get the lower case ad type if it exists:
         if (numberOfArguments >= 1 && luaState.isTable(1)) {
             
