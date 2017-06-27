4b7954ae1075591e7e84b6434deb7dac223afc71
kosyakow <kosyakow@gmail.com>
2017-06-27 18:46:43 +0300

    Update iphoneBuild.sh

--- iphoneBuild.sh
+++ iphoneBuild.sh
@@ -1,8 +1,7 @@
 set -ve
 cd ..
 [ -d ./corona-test-app ] && rm -rf ./corona-test-app
-git clone git@github.com:Vungle/corona-test-app.git
-git checkout sdk5
+git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
 cd coronaplugin
 cd ios
 ./build.sh
