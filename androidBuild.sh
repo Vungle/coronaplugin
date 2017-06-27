34755ebec4e926672fa10692f4a9db7e3ecb2c45
kosyakow <kosyakow@gmail.com>
2017-06-27 18:37:26 +0300

    Update androidBuild.sh

--- androidBuild.sh
+++ androidBuild.sh
@@ -3,6 +3,7 @@ pwd
 cd ..
 [ -d ./corona-test-app ] && rm -rf ./corona-test-app
 git clone git@github.com:Vungle/corona-test-app.git
+git checkout sdk5
 cd coronaplugin
 cd android
 ./build.sh ~/Library/Android/sdk/
