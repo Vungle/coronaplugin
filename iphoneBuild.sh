be9aa05875c6940ebca1f701095a2e778a817bf8
AndreyK <kosyakow@gmail.com>
2018-04-10 14:55:19 +0300

    Fix scripts

--- iphoneBuild.sh
+++ iphoneBuild.sh
@@ -1,9 +1,9 @@
 set -ve
 cd ..
-#[ -d ./corona-test-app ] && rm -rf ./corona-test-app
-#git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
+[ -d ./corona-test-app ] && rm -rf ./corona-test-app
+git clone -b sdk5 git@github.com:Vungle/corona-test-app.git
 cd coronaplugin
 cd ios
 ./build.sh
 cd ..
-#[ -d ../corona-test-app ] && rm -rf ../corona-test-app
+[ -d ../corona-test-app ] && rm -rf ../corona-test-app
