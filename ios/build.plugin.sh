0b5017c54d33847b6918dd735f86557349d9a0a2
AndreyK <kosyakow@gmail.com>
2018-04-11 13:21:12 +0300

    Fix build scripts

--- ios/build.plugin.sh
+++ ios/build.plugin.sh
@@ -10,10 +10,10 @@ sub="@\"$_pluginVersion\";//plugin version. Do not delete this comment"
 sed -E -i .bak "s#@\"[0-9]+_[0-9]+_[0-9]+\";//plugin version. Do not delete this comment#$sub#g" Plugin/VungleAds.mm
 rm Plugin/VungleAds.mm.bak
 
-xcodebuild -target plugin_vungle -sdk iphoneos${IOS_SDK} -project Plugin.xcodeproj clean
-xcodebuild -target plugin_vungle -sdk iphonesimulator${IOS_SDK} -project Plugin.xcodeproj clean
-xcodebuild -target plugin_vungle -sdk iphoneos${IOS_SDK} -project Plugin.xcodeproj build
-xcodebuild -target plugin_vungle -sdk iphonesimulator${IOS_SDK} -project Plugin.xcodeproj build
+xcodebuild -target plugin_vungle -project Plugin.xcodeproj clean
+xcodebuild -target plugin_vungle -project Plugin.xcodeproj clean
+xcodebuild -target plugin_vungle -project Plugin.xcodeproj build
+xcodebuild -target plugin_vungle -project Plugin.xcodeproj build
 for version in $CORONA_RELEASES; do
 	cp build/Release-iphoneos/libplugin_vungle.a ../plugins/${version}/iphone/libplugin_vungle.a
 	cp build/Release-iphonesimulator/libplugin_vungle.a ../plugins/${version}/iphone-sim/libplugin_vungle.a
